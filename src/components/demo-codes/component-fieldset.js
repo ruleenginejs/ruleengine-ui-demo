f1f3a953c19f4e75ae424626742b74d5c965823e
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2021-12-14 12:18:11 +0300

    Add title actions demo

--- src/components/demo-codes/component-fieldset.js
+++ src/components/demo-codes/component-fieldset.js
@@ -21,7 +21,7 @@ const fieldset_basic_usage_html = `
 </template>
 `;
 
-const fieldset_vertical_layout_html = `
+const fieldset_vertical_field_layout_html = `
 <template>
   <v-fieldset label="Title">
     <v-field-layout vertical>
@@ -44,6 +44,28 @@ const fieldset_vertical_layout_html = `
 </template>
 `;
 
+const fieldset_title_actions_html = `
+<template>
+  <v-fieldset label="Title">
+    <template #label-actions>
+      <v-action-list>
+        <v-action-item icon="add" title="Add" />
+        <v-action-item icon="history" title="History" />
+        <v-action-item icon="trash" title="Remove" />
+      </v-action-list>
+    </template>
+    <v-field-layout>
+      <template #label>
+        <v-label truncate>Label 1</v-label>
+      </template>
+      <template #value>
+        <v-input placeholder="Enter your text" />
+      </template>
+    </v-field-layout>
+  </v-fieldset>
+</template>
+`;
+
 export default [
   {
     name: "fieldset_basic_usage_html",
@@ -51,8 +73,13 @@ export default [
     lang: "html"
   },
   {
-    name: "fieldset_vertical_layout_html",
-    code: fieldset_vertical_layout_html,
+    name: "fieldset_vertical_field_layout_html",
+    code: fieldset_vertical_field_layout_html,
+    lang: "html"
+  },
+  {
+    name: "fieldset_title_actions_html",
+    code: fieldset_title_actions_html,
     lang: "html"
   }
 ]
--- src/components/demo-component-fieldset.vue
+++ src/components/demo-component-fieldset.vue
@@ -26,7 +26,7 @@
     </template>
   </demo-block>
 
-  <h3>Vertical Layout</h3>
+  <h3>Vertical Field Layout</h3>
   <demo-block class="demo-fieldset-block">
     <v-fieldset label="Title">
       <v-field-layout vertical>
@@ -47,7 +47,31 @@
       </v-field-layout>
     </v-fieldset>
     <template #code>
-      <demo-code name="fieldset_vertical_layout_html" />
+      <demo-code name="fieldset_vertical_field_layout_html" />
+    </template>
+  </demo-block>
+
+  <h3>Fieldset Title Actions</h3>
+  <demo-block class="demo-fieldset-block">
+    <v-fieldset label="Title">
+      <template #label-actions>
+        <v-action-list>
+          <v-action-item icon="add" title="Add" />
+          <v-action-item icon="history" title="History" />
+          <v-action-item icon="trash" title="Remove" />
+        </v-action-list>
+      </template>
+      <v-field-layout>
+        <template #label>
+          <v-label truncate>Label 1</v-label>
+        </template>
+        <template #value>
+          <v-input placeholder="Enter your text" />
+        </template>
+      </v-field-layout>
+    </v-fieldset>
+    <template #code>
+      <demo-code name="fieldset_title_actions_html" />
     </template>
   </demo-block>
 </template>
